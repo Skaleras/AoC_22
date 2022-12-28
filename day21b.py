5a7d6672ca7282a2ebad5f37fd9221d8cfaa63f2
Victor H. Leras <65517984+Skaleras@users.noreply.github.com>
2022-12-28 14:44:54 -0400

    day21

---
+++ day21b.py
@@ -0,0 +1,27 @@
+import sympy
+
+monkeys = { "humn": sympy.Symbol("x") }
+
+x = [line.strip() for line in open('input_day21.txt')]
+
+ops = {
+    "+": lambda x, y: x + y,
+    "-": lambda x, y: x - y,
+    "*": lambda x, y: x * y,
+    "/": lambda x, y: x / y,
+}
+
+for a in x:
+    name, expr = a.split(": ")
+    if name in monkeys: continue
+    if expr.isdigit():
+        monkeys[name] = sympy.Integer(expr)
+    else:
+        left, op, right = expr.split()
+        if left in monkeys and right in monkeys:
+            if name == "root":
+                print(sympy.solve(monkeys[left] - monkeys[right])[0])
+                break
+            monkeys[name] = ops[op](monkeys[left], monkeys[right])
+        else:
+            x.append(a)
